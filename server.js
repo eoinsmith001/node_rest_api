350601db8ab87a5ba4d625b29d11a97e7f635524
Eoin Smith <eoinsmith001@gmail.com>
2014-11-12 17:13:06 +1000

    CRUD REST routing added to server.js

--- server.js
+++ server.js
@@ -14,12 +14,76 @@ mongoose.connect('mongodb://localhost:27017');
 
 // api routes
 var router = express.Router();
+
+// use this middleware for all requests!
+router.use( function(req,res,next) {
+  console.log( "something is happening" );
+  next();
+});
+
 // test route
 router.get('/', function(req,res) {
   res.json( {message:"wecome to the api"} );
 });
 
+// routes ending in /bears
+router.route( '/bears' )
+  // Create / POST
+  .post(function(req,res) {
+    var bear = new Bear();
+    bear.name = req.body.name;
+    // save and err check
+    bear.save( function(err) {
+      if (err)
+        res.send(err);
+      res.json({message: "Bear Created!"});
+    });
+  })
+  // GET
+  .get(function(req,res) {
+    Bear.find(function(err,bears){
+      if (err)
+        res.send(err);
+      res.json(bears);
+    });
+  });
+
+// routes ending in /bears/:bear_id
+router.route( '/bears/:bear_id' )
+  .get(function(req,res) {
+    // get a single bear by id
+    Bear.findById(req.params.bear_id, function(err,bear) {
+      if (err)
+        res.send(err);
+      res.json(bear);
+    });
+  })
+  .put(function(req,res) {
+    // update a bear by id
+    Bear.findById(req.params.bear_id, function(err,bear) {
+      if (err)
+        res.send(err);
+      console.log(bear);
+      bear.name = req.body.name;  // update
+      bear.save(function(err) {
+	if (err)
+	  res.send(err);
+	res.json({message: "Bear updated, " + bear.name});
+      });
+    });
+  })
+  .delete(function(req,res) {
+    Bear.remove({
+      _id: req.params.bear_id
+    }, function(err,bear) {
+      if(err)
+	res.send(err);
+      res.json({message: "Successful Delete"});
+    });
+  });
+
 // register routes
 app.use('/api', router);
 app.listen(port);
 console.log( "Application running on port " + port );
+// ----
